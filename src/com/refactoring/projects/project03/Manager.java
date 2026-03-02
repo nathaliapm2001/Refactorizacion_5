a138ef8b44f2ae74ccce431b77b4756a843506dd
Nathalia <nathaliapm2001@gmail.com>
2026-03-02 23:59:22 +0100

    Refactor: Código final con atributos privados

--- src/com/refactoring/projects/project03/Manager.java
+++ src/com/refactoring/projects/project03/Manager.java
@@ -2,12 +2,53 @@ package com.refactoring.projects.project03;
 
 public class Manager {
 	
-    public String nombre;
-    public double salario;
-    public String departamento;
-    public int empleadosACargo;
+    private static final int MESES_DEL_ANIO = 12;
+	private static final int BONO_POR_EMPLEADO = 1000;
+	private String nombre;
+    private double salario;
+    private String departamento; 
+    private int empleadosACargo;
     
-    public void calcularSalarioAnual() {
-        System.out.println(salario * 12 + empleadosACargo * 1000);
+    public double calcularSalarioAnual() {
+        return getSalario() * MESES_DEL_ANIO + calcularBono();
     }
+
+	private int calcularBono() {
+		return getEmpleadosACargo() * BONO_POR_EMPLEADO;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+
+	public String getDepartamento() {
+		return departamento;
+	}
+
+	public void setDepartamento(String departamento) {
+		this.departamento = departamento;
+	}
+
+	public int getEmpleadosACargo() {
+		return empleadosACargo;
+	}
+
+	public void setEmpleadosACargo(int empleadosACargo) {
+		this.empleadosACargo = empleadosACargo;
+	}
+
+
 }
+
